921b68a40073a414eabd6f0e9b8fb77015197049
hi-vishal <114975029+hi-vishal@users.noreply.github.com>
2023-05-09 22:51:57 +0530

    Update left_rotation.cpp

--- Assignment_3/left_rotation.cpp
+++ Assignment_3/left_rotation.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+    int n,d;
+    cin>>n>>d;
+    int arr[n];
+    vector<int> vec(n);
+    for(int i=0;i<n;i++){
+        cin>>vec[i];
+    }
+    for(int i=d;i<n;i++){
+        cout<<vec[i]<<" ";
+    }
+    for(int i=0;i<d;i++){
+        cout<<vec[i]<<" ";
+    }
+    return 0;
+}
